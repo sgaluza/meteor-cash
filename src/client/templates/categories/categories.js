e926c2c438cede7144e7f2c38e3ab46472de0121
Sergey Galuza <sergey@galuza.com>
2015-07-27 18:12:54 +0300

    bootstrap tree

--- src/client/templates/categories/categories.js
+++ src/client/templates/categories/categories.js
@@ -8,6 +8,41 @@ Template.categoryEditModal.onRendered(function(){
     $('input[name=title]').focus();
 });
 
+Template.categories.onRendered(function(){
+    $('#tree').treeview({data:  [
+        {
+            text: "Parent 1",
+            nodes: [
+                {
+                    text: "Child 1",
+                    nodes: [
+                        {
+                            text: "Grandchild 1"
+                        },
+                        {
+                            text: "Grandchild 2"
+                        }
+                    ]
+                },
+                {
+                    text: "Child 2"
+                }
+            ]
+        },
+        {
+            text: "Parent 2"
+        },
+        {
+            text: "Parent 3"
+        },
+        {
+            text: "Parent 4"
+        },
+        {
+            text: "Parent 5"
+        }
+    ]});
+});
 
 Template.categories.events({
     "click #add-category": function() {
